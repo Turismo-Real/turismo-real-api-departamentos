3b6a02e431a32608bbad79501150ba6c99671f43
Enzo Norese <enzo.norese@gmail.com>
2021-10-18 00:11:33 -0300

    Se agregan logs

--- TurismoReal_Departamentos.Api/Controllers/DepartamentoController.cs
+++ TurismoReal_Departamentos.Api/Controllers/DepartamentoController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using TurismoReal_Departamentos.Core.DTOs;
 using TurismoReal_Departamentos.Core.Interfaces;
+using TurismoReal_Departamentos.Core.Log;
 using TurismoReal_Departamentos.Core.Messages.Output;
 
 namespace TurismoReal_Departamentos.Api.Controllers
@@ -12,6 +14,8 @@ namespace TurismoReal_Departamentos.Api.Controllers
     public class DepartamentoController : ControllerBase
     {
         public readonly IDepartamentoRepository _departamentoRepository;
+        private readonly string serviceName = "turismo_real_departamentos";
+        private LogModel log;
 
         public DepartamentoController(IDepartamentoRepository departamentoRepository)
         {
@@ -22,7 +26,15 @@ namespace TurismoReal_Departamentos.Api.Controllers
         [HttpGet]
         public async Task<List<Departamento>> GetDepartamentos()
         {
+            log = new LogModel();
+            log.InitLog(serviceName, "GET", "/api/v1/departamento", DateTime.Now);
+
             List<Departamento> deptos = await _departamentoRepository.GetDepartamentos();
+
+            // LOG
+            log.EndLog(DateTime.Now, 200, "Lista departamentos");
+            Console.WriteLine(log.parseJson());
+            // LOG
             return deptos;
         }
 
@@ -30,13 +42,25 @@ namespace TurismoReal_Departamentos.Api.Controllers
         [HttpGet("{id}")]
         public async Task<object> GetDepartamento(int id)
         {
+            log = new LogModel();
+            log.InitLog(serviceName, "GET", "/api/v1/departamento/{id}", DateTime.Now);
+
             Departamento depto = await _departamentoRepository.GetDepartamento(id);
 
             if(depto.id_departamento == 0)
             {
-                return new NotFoundOutput($"No se encontró departamento con ID {id}");
+                NotFoundOutput response = new NotFoundOutput($"No se encontró departamento con ID {id}");
+                // LOG
+                log.EndLog(DateTime.Now, 200, response);
+                Console.WriteLine(log.parseJson());
+                // LOG
+                return response;
             }
 
+            // LOG
+            log.EndLog(DateTime.Now, 200, depto);
+            Console.WriteLine(log.parseJson());
+            // LOG
             return depto;
         }
 
@@ -44,16 +68,29 @@ namespace TurismoReal_Departamentos.Api.Controllers
         [HttpPost]
         public async Task<CreateOutput> CreateDepartamento([FromBody] Departamento payload)
         {
+            log = new LogModel();
+            log.InitLog(serviceName, "POST", "/api/v1/departamento", DateTime.Now);
+
             int result = await _departamentoRepository.CreateDepartamento(payload);
 
             if(result > 0)
             {
                 Departamento depto = await _departamentoRepository.GetDepartamento(result);
-                return new CreateOutput("Departamento creado exitosamente.", depto);
+                CreateOutput response = new CreateOutput("Departamento creado exitosamente.", depto);
+                // LOG
+                log.EndLog(DateTime.Now, 200, response);
+                Console.WriteLine(log.parseJson());
+                // LOG
+                return response;
             }
             else
             {
-                return new CreateOutput("Error al crear departamento", null);
+                CreateOutput response = new CreateOutput("Error al crear departamento", null);
+                // LOG
+                log.EndLog(DateTime.Now, 200, response);
+                Console.WriteLine(log.parseJson());
+                // LOG
+                return response;
             }
         }
 
@@ -61,24 +98,42 @@ namespace TurismoReal_Departamentos.Api.Controllers
         [HttpPut("{id}")]
         public async Task<object> UpdateDepartamento(int id, [FromBody] Departamento depto)
         {
+            log = new LogModel();
+            log.InitLog(serviceName, "PUT", "/api/v1/departamento/{id}", DateTime.Now);
+
             int updated = await _departamentoRepository.UpdateDepartamento(id, depto);
 
             if (updated == 0) return new { message = "Error al actualizar departamento.", updated = false };
             if (updated == -1) return new { message = $"No existe departamento con ID {id}.", updated = false };
 
             Departamento newDepto = await _departamentoRepository.GetDepartamento(id);
-            return new { message = "Departamento actualizado.", updated = true, departamento = newDepto };
+            var response = new { message = "Departamento actualizado.", updated = true, departamento = newDepto };
+
+            // LOG
+            log.EndLog(DateTime.Now, 200, response);
+            Console.WriteLine(log.parseJson());
+            // LOG
+            return response;
         }
 
         // DELETE: /api/v1/departamento/{id}
         [HttpDelete("{id}")]
         public async Task<DeleteOutput> DeleteDepartamento(int id)
         {
+            log = new LogModel();
+            log.InitLog(serviceName, "DELETE", "/api/v1/departamento/{id}", DateTime.Now);
+
             int removed = await _departamentoRepository.DeleteDepartamento(id);
 
             if(removed == 0) return new DeleteOutput("Error al eliminar departamento.", false);
             if(removed == -1) return new DeleteOutput($"No existe el departamento con ID {id}", false);
-            return new DeleteOutput("Departamento eliminado.", true);
+
+            DeleteOutput response = new DeleteOutput("Departamento eliminado.", true);
+            // LOG
+            log.EndLog(DateTime.Now, 200, response);
+            Console.WriteLine(log.parseJson());
+            // LOG
+            return response;
         }
 
     }
--- TurismoReal_Departamentos.Core/Log/LogModel.cs
+++ TurismoReal_Departamentos.Core/Log/LogModel.cs
@@ -9,11 +9,29 @@ namespace TurismoReal_Departamentos.Core.Log
         public string level = "INFO";
         public int statusCode = 0;
         public string servicio = string.Empty;
+        public string method = string.Empty;
+        public string endpoint = string.Empty;
         public DateTime inicioSolicitud;
         public DateTime finSolicitud;
         public string tiempoSolicitud;
         public object payload = null;
         public object response = null;
+
+        public void InitLog(string service, string method, string path, DateTime startRequest)
+        {
+            servicio = service;
+            this.method = method;
+            endpoint = path;
+            inicioSolicitud = startRequest;
+        }
+
+        public void EndLog(DateTime endRequest, int status, object response)
+        {
+            finSolicitud = endRequest;
+            tiempoSolicitud = (finSolicitud - inicioSolicitud).TotalMilliseconds + "ms";
+            statusCode = status;
+            this.response = response;
+        }
 
         public string parseJson()
         {
